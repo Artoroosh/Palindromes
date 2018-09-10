22f716b76f5e44bb4e5b9862858dcb4c5e2f6019
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-09-10 15:11:57 -0400

    commit01

---
+++ src/palindrome/Palindrome.java
@@ -0,0 +1,21 @@
+/*
+ * Corbin Nash
+ * Started: 10/09/2018
+ * 
+ */
+package palindrome;
+import javax.swing.*;
+public class Palindrome {
+  public static void main(String[] args) {
+  String primaryInput = JOptionPane.showInputDialog
+ ("Words that are arranged the same way forwards as they are backwards are called palindromes"+ "\n\nEnter a Word");
+  
+  
+  
+  
+  if (primaryInput = "")
+  else {
+      System.out.println("That is not a palindromes");
+      }
+    }  
+}
